d10dc7f9530e1685807688a2bcc95f70589caeb5
Mohammad Sharfuddin Khan <79760546+sharfuddin1984@users.noreply.github.com>
2024-10-14 13:51:28 +0530

    angular test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+  textHeading:string = "Drink Water";  
+
+  waterLevel: number = 0;
+  maxLimit: number = 1000;
+
+  addWater() {
+    if (this.waterLevel < this.maxLimit) {
+      this.waterLevel += 100;
+    }
+  }
+  removeWater() {
+    if (this.waterLevel >= 100) {
+      this.waterLevel -= 100;
+    }
+  }
 }
